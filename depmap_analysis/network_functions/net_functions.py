072dc9ba9e314fd3283eecc70ab4592225f5cc7c
kkaris <karis.klas@gmail.com>
2021-09-20 16:40:13 -0400

    Also set z_score, corr_weight

--- depmap_analysis/network_functions/net_functions.py
+++ depmap_analysis/network_functions/net_functions.py
@@ -690,7 +690,8 @@ def sif_dump_df_to_digraph(df: Union[pd.DataFrame, str],
                           'belief': 1.0, 'weight': NP_PRECISION,
                           'curated': True,
                           'english': f'{pns}:{pid} is an ontological parent '
-                                     f'of {ns}:{_id}'}
+                                     f'of {ns}:{_id}',
+                          'z_score': 0, 'corr_weight': 1}
                     # Add non-existing nodes
                     if ed['agA_name'] not in indranet_graph.nodes:
                         indranet_graph.add_node(ed['agA_name'],
