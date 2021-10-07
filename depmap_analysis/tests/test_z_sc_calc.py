963d560c481bcbaa87616b0cf575dc0175ef8b5a
kkaris <karis.klas@gmail.com>
2021-10-07 13:07:29 -0400

    Update z-score test

--- depmap_analysis/tests/test_z_sc_calc.py
+++ depmap_analysis/tests/test_z_sc_calc.py
@@ -1,45 +1,65 @@
+from pathlib import Path
+
 import numpy as np
 import pandas as pd
-from depmap_analysis.util.statistics import *
+
 from depmap_analysis.preprocessing.depmap_preprocessing import run_corr_merge
+from depmap_analysis.util.statistics import *
 from . import *
 
 
 def test_merge_corr():
-    size = 50
-    a, nan_count_a = _get_df_w_nan(size=size)
-    acols = [str(cn) for cn in a.columns]
-    a.columns = acols
-    a.index = acols
+    shape = 50, 10
+    a, nan_count_a = _get_raw_w_nan(shape, nan_count=30)
+    a.index = [f'{str(cn)} {str(cn)}' for cn in a.index]
+    a.columns = [str(cn) for cn in a.columns]
 
-    b, nan_count_b = _get_df_w_nan(size=size)
-    bcols = [str(cn) for cn in b.columns]
-    b.columns = bcols
-    b.index = bcols
+    b, nan_count_b = _get_raw_w_nan(shape, nan_count=30)
+    b.index = [f'{str(cn)} {str(cn)}' for cn in b.index]
+    b.columns = [str(cn) for cn in b.columns]
 
     # Get samples
     an = get_n(recalculate=True, data_df=a)
     bn = get_n(recalculate=True, data_df=b)
 
+    # Make corr matrices
+    a_corr = a.corr()
+    b_corr = b.corr()
+
     # Get logp
-    alog = get_logp(recalculate=True, data_corr=a, data_n=an)
-    blog = get_logp(recalculate=True, data_corr=b, data_n=bn)
+    alog = get_logp(recalculate=True, data_corr=a_corr, data_n=an)
+    blog = get_logp(recalculate=True, data_corr=b_corr, data_n=bn)
 
     # Get z
-    az = get_z(recalculate=True, data_logp=alog, data_corr=a)
-    bz = get_z(recalculate=True, data_logp=blog, data_corr=b)
+    az = get_z(recalculate=True, data_logp=alog, data_corr=a_corr)
+    bz = get_z(recalculate=True, data_logp=blog, data_corr=b_corr)
 
+    # Merge with stouffer's
     stouffer_merged: pd.DataFrame = (az + bz) / np.sqrt(2)
-    merged: pd.DataFrame = run_corr_merge(crispr_corr=a, rnai_corr=b,
-                                          output_dir='temp')
+
+    # Drop nan's (this is default behavior in run_corr_merge)
+    stouffer_merged = stouffer_merged.dropna(
+        axis=0, how='all').dropna(axis=1, how='all')
+
+    # Set diagonal to NaN
+    np.fill_diagonal(a=stouffer_merged.values, val=np.nan)
+
+    Path('temp').mkdir(exist_ok=True)
+    afp = 'temp/a_raw.h5'
+    bfp = 'temp/b_raw.h5'
+    a.to_hdf(afp, 'test')
+    b.to_hdf(bfp, 'test')
+    merged: pd.DataFrame = run_corr_merge(
+        crispr_raw=afp, rnai_raw=bfp, corr_output_dir="temp"
+    )
 
     # Check inf count
-    assert (stouffer_merged.abs() == np.inf).sum().sum() == \
-        (merged.abs() == np.inf).sum().sum()
+    assert (stouffer_merged.abs() == np.inf).sum().sum() == (
+        merged.abs() == np.inf
+    ).sum().sum()
 
     # Check NaN count
-    assert pd.isna(stouffer_merged).sum().sum() == \
-        pd.isna(merged).sum().sum()
+    assert pd.isna(stouffer_merged).sum().sum() == pd.isna(merged).sum().sum()
 
     # Are they the same?
     assert stouffer_merged.equals(merged)
